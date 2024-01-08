4323811b9a249550d5c5c416ec838e03e8510702
ibtissam <ibtissamelhani36@gmail.com>
2024-01-08 09:17:57 +0100

    first commit

---
+++ app/database/connection.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace app\connection;
+
+use PDO;
+use Dotenv\Dotenv;
+require_once __DIR__.'/../../vendor/autoload.php';
+
+
+$dotenv =Dotenv::createImmutable(__DIR__.'/../../');
+$dotenv->load();
+
+class Connection
+{
+
+    private static $instance;
+    private  $connection;
+
+    private function __construct(){
+
+            $servername = $_ENV['DB_HOST'];
+            $username = $_ENV['DB_USER'];
+            $password = $_ENV['DB_PASSWORD'];
+            $dbname = $_ENV['DB_NAME'];
+            try{
+                $this->connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+                echo "done";
+            }catch (PDOException $e) {
+                die("Connection failed: " . $e->getMessage());
+            } 
+        }
+        public static function getInstence(){
+            if(!isset(self::$instance)){
+                self::$instance = new Connection();
+            }
+            return self::$instance;
+        }
+
+        public function getConnect(){
+            return $this->connection;
+        }
+}
+
+$db = Connection::getInstence()->getConnect();
+
+?>
+
---
+++ app/database/script.sql
@@ -0,0 +1,55 @@
+-- Active: 1704309211331@@127.0.0.1@3306@wikizone
+CREATE DATABASE wikizone;
+
+CREATE TABLE roles (
+    id int PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50)
+);
+
+
+CREATE TABLE users (
+    id int PRIMARY KEY AUTO_INCREMENT,
+    firstName VARCHAR(255),
+    lastName VARCHAR(255),
+    userName VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255),
+    profile VARCHAR(255),
+    role_id int ,
+    Foreign Key (role_id) REFERENCES roles(id) ON DELETE CASCADE ON UPDATE CASCADE
+); 
+
+CREATE TABLE categories (
+    id int PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50)
+);
+
+CREATE TABLE tags (
+    id int PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50),
+    category_id int,
+    Foreign Key (category_id) REFERENCES categories(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE wikis (
+    id int PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255),
+    description VARCHAR(255),
+    content VARCHAR(255),
+    date date  DEFAULT CURRENT_TIMESTAMP,
+    status VARCHAR(50),
+    photo VARCHAR(255),
+    writer int ,
+    Foreign Key (writer) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE
+); 
+
+CREATE TABLE wiki_tags (
+    wiki_id int,
+    tag_id int,
+    PRIMARY KEY (wiki_id, tag_id),
+    Foreign Key (wiki_id) REFERENCES wikis(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    Foreign Key (tag_id) REFERENCES tags(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+
