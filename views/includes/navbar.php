362b770fb14eede15c9b2eb4dc7a530713048b62
ibtissam <ibtissamelhani36@gmail.com>
2024-01-09 21:17:54 +0100

    home page modified

--- views/includes/navbar.php
+++ views/includes/navbar.php
@@ -9,8 +9,17 @@
 </head>
 <body>
     
+<style>
+    * {
 
-<nav class="bg-neutral-100 dark:bg-gray-900  w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+      font-family: 'Roboto Condensed', sans-serif;
+      
+
+    }
+</style>
+  
+
+<nav class="bg-white fixed bg-opacity-25 dark:bg-gray-900  w-full z-20 top-0 start-0  dark:border-gray-600">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <a href="home" class="flex items-center space-x-3 rtl:space-x-reverse">
         <img src="/wikizone/public/imgs/logo.png" class="h-8" alt="wikizone Logo">
@@ -18,11 +27,18 @@
     </a>
     <div class="flex md:order-2 space-x-3 items-center gap-8 md:space-x-0 rtl:space-x-reverse">
         <a href="home" class="text-md font-semibold hover:text-orange-600 ">Home</a>
-        <a  href="signin" class="text-md text-orange-700 underline decoration-solid font-semibold hover:text-orange-600 ">Sign in</a>
+        <a  href="signin" class="text-md  underline decoration-solid font-semibold hover:text-orange-600 ">Sign in</a>
         <a href="signup" class="text-md">
             <button type="button" class=" text-white bg-orange-700 hover:bg-orange-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign up</button>
         </a>
+        <!-- <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+        <span class="sr-only">Open main menu</span>
+        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+        </svg>
+    </button> -->
     </div>
+    
   </div>
 </nav>
 
