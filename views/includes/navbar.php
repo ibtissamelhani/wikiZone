29d341699b13f8a3b000e73d1b134576506fecec
ibtissam <ibtissamelhani36@gmail.com>
2024-01-12 17:24:10 +0100

    responsive navbar

--- views/includes/navbar.php
+++ views/includes/navbar.php
@@ -30,38 +30,54 @@
     </style>
 
 
-    <nav class="bg-white fixed bg-opacity-25 dark:bg-gray-900  w-full z-20 top-0 start-0  dark:border-gray-600">
+    <nav class="bg-white fixed bg-opacity-25   w-full z-20 top-0 start-0 h-18">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+           
             <a href="home" class="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src="/wikizone/public/imgs/logo.png" class="h-8" alt="wikizone Logo">
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">WikiZone</span>
+                <img src="/wikizone/public/imgs/logo.png" class="h-8 sm:h-10 md:h-12 lg:h-10 xl:h-10"
+                    alt="WikiZone Logo">
+                <span
+                    class="self-center text-2xl  sm:text-xl md:text-2xl  font-semibold whitespace-nowrap ">WikiZone</span>
             </a>
-            <div class="flex md:order-2 space-x-3 items-center gap-8 md:space-x-0 rtl:space-x-reverse">
 
-                <a href="home" class="text-md font-semibold hover:text-orange-600 ">Home</a>
-                <?php if(!isset($_SESSION['loggedIn'])) : ?>
-                <a href="signin" class="text-md  underline decoration-solid font-semibold hover:text-orange-600 ">Sign
-                    in</a>
-                <a href="signup" class="text-md">
-                    <button type="button"
-                        class=" text-white bg-orange-700 hover:bg-orange-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign
-                        up</button>
-                </a>
+            <button data-collapse-toggle="navbar-default" type="button"
+                class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                aria-controls="navbar-default" aria-expanded="false">
+                <span class="sr-only">Open main menu</span>
+                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 17 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M1 1h15M1 7h15M1 13h15" />
+                </svg>
+            </button>
+            <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+                <ul
+                    class="font-medium flex flex-col  p-4 md:p-0 mt-4 gap-4 items-center  md:flex-row  md:space-x-8 rtl:space-x-reverse ">
+                    <li>
+                        <a href="home" class="text-md font-semibold hover:text-orange-600 ">Home</a>
 
-
-
-
-                <!-- <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
-        <span class="sr-only">Open main menu</span>
-        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-        </svg>
-    </button> -->
-            </div>
-            <?php else : ?>
-            <a href="profile?id=<?=$_SESSION['userId']?>"
-                class="text-md underline decoration-solid font-semibold hover:text-orange-600 ">My wikis</a>
-            <div class="flex items-center">
+                    </li>
+                    <?php if(!isset($_SESSION['loggedIn'])) : ?>
+                    <li>
+                        <a href="signin"
+                            class="text-md  underline decoration-solid font-semibold hover:text-orange-600 ">Sign
+                            in</a>
+                    </li>
+                    <li>
+                        <a href="signup" class="text-md">
+                            <button type="button"
+                                class=" text-white bg-orange-700 hover:bg-orange-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign
+                                up</button>
+                        </a>
+                    </li>
+                    <?php else : ?>
+                    <li>
+                        <a href="profile?id=<?=$_SESSION['userId']?>"
+                            class="text-md underline decoration-solid font-semibold hover:text-orange-600 ">My
+                            wikis</a>
+                    </li>
+                    <li>
+                    <div class="flex items-center">
                 <div class="flex items-center ms-3">
                     <div>
                         <button type="button"
@@ -86,6 +102,10 @@
                     </div>
                 </div>
             </div>
+                    </li>
+                </ul>
+            </div>
+
         </div>
         <?php endif; ?>
         </div>
